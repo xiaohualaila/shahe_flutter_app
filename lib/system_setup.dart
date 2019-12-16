fd7db3601e9e634b4ff3dcd356f784bbdf44d6c4
15129871365 <380129462@qq.com>
2019-12-16 17:29:08 +0800

    修改更新

---
+++ lib/system_setup.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class SetupPage extends StatefulWidget {
+  @override
+  _SetupPageState createState() => _SetupPageState();
+}
+
+class _SetupPageState extends State<SetupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '系统设置',
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(top: 20,left: 20,right:20),
+        child: Column(
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    '本版更新',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Text(
+                  '版本号v1.0',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.green,
+                )
+              ],
+            ),
+            Divider(
+              color: Color(0xFFE0E0E0),
+              height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    '关于软件',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.green,
+                )
+              ],
+            ),
+            Divider(
+              color: Color(0xFFE0E0E0),
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
